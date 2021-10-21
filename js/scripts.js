93c24cd2459a7a77b5fd42268fef813e5d7c36cd
Chase-Stokes <chasehstokes@gmail.com>
2021-10-20 17:15:09 -0700

    add a card displaying a recipt
    Co-authored-by: Matt Casperson <mc.casperson@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,10 @@ $(document).ready(function() {
     const name = $("#yourName").val();
     const address1 = $("#street").val();
     const address2 = $("#city").val();
+    $(".yourName").text(name);
+    $(".street").text(address1);
+    $(".city").text(address2);
+    $("#output").show();
 
   });
 });
